92f88e85ee8d929fa0ef4c4b0627b00b019ef700
ivan <jperdomo@sre.gob.mx>
2020-07-08 23:03:11 -0500

    reportes etiqueta vista

--- resources/js/routes/Reports.js
+++ resources/js/routes/Reports.js
@@ -1,6 +1,7 @@
 import {RouterView} from './RouterView.js';
 import ReportIndex from '../components/views/reports/Index';
 import Record from '../components/views/reports/Record/Record';
+import LabelIndex from '../components/views/reports/label/index';
 
 export default {
     path: '/reportes',
@@ -15,6 +16,11 @@ export default {
             path: 'expedientes',
             component: Record,
             name: 'Record',
+        },
+        {
+            path: 'etiqueta',
+            component: LabelIndex,
+            name: 'LabelIndex',
         },
     ],
 }
