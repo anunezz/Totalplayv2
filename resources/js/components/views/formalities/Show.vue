c002d1ad40a8ea8a453168c60b0ce62f0ff14ba1
Pedro Dominguez Diaz <peter.slash16@gmail.com>
2020-08-05 02:01:59 -0500

    metodos repository

---
+++ resources/js/components/views/formalities/Show.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <header-section icon="fas fa-file-signature" title="Consulta de trámite">
+            <template slot="buttons">
+                <el-button
+                    size="small"
+                    type="danger"
+                    icon="el-icon-arrow-left"
+                    @click="$router.push({name: 'ListFormalities' })">
+                    Regresar
+                </el-button>
+            </template>
+        </header-section>
+    </div>
+</template>
+
+<script>
+import HeaderSection from "../layouts/partials/HeaderSection";
+
+export default {
+    components: {
+        HeaderSection,
+    },
+    data(){
+        return{
+
+        }
+    },
+    created() {
+        if (this.$route.params.id !== undefined) this.showRegister(this.$route.params.id);
+    },
+    methods:{
+        showRegister(id){
+            console.log('ver el registro',id)
+            axios.get(`/api/formalities/${id}`).then(response => {
+                console.log(response)
+            }).catch(error => {
+                this.$message({
+                    type: "warning",
+                    message: "No fue posible completar la acción, intente nuevamente."
+                });
+            });
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
